0c9a6e9f1cd998c204a2a922e4f6eee4c35cac23
Simon Franco Angarita <zaimon88@gmail.com>
2023-11-08 15:53:39 -0500

    Sidebar Item

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -13,7 +13,7 @@ import { useParams, usePathname, useRouter } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { useMutation } from "convex/react";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 
 import { cn } from "@/lib/utils";
 import { api } from "@/convex/_generated/api";
@@ -26,19 +26,19 @@ import { api } from "@/convex/_generated/api";
 // import { useSettings } from "@/hooks/use-settings";
 
 import { UserItem } from "./user-item";
-// import { Item } from "./item";
+import { Item } from "./item";
 // import { DocumentList } from "./document-list";
 // import { TrashBox } from "./trash-box";
 // import { Navbar } from "./navbar";
 
 export const Navigation = () => {
-  //   const router = useRouter();
+  const router = useRouter();
   //   const settings = useSettings();
   //   const search = useSearch();
   //   const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  //   const create = useMutation(api.documents.create);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -122,16 +122,17 @@ export const Navigation = () => {
     }
   };
 
-  //   const handleCreate = () => {
-  //     const promise = create({ title: "Untitled" })
-  //       .then((documentId) => router.push(`/documents/${documentId}`))
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
-  //     toast.promise(promise, {
-  //       loading: "Creating a new note...",
-  //       success: "New note created!",
-  //       error: "Failed to create a new note."
-  //     });
-  //   };
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <>
@@ -155,30 +156,24 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
-          {/* <Item
+          <Item
             label="Search"
             icon={Search}
             isSearch
-            onClick={search.onOpen}
+            // onClick={search.onOpen}
           />
           <Item
             label="Settings"
             icon={Settings}
-            onClick={settings.onOpen}
+            // onClick={settings.onOpen}
           />
-          <Item
-            onClick={handleCreate}
-            label="New page"
-            icon={PlusCircle}
-          />
+          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
+
+        {/* 
         <div className="mt-4">
           <DocumentList />
-          <Item
-            onClick={handleCreate}
-            icon={Plus}
-            label="Add a page"
-          />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
           <Popover>
             <PopoverTrigger className="w-full mt-4">
               <Item label="Trash" icon={Trash} />
@@ -189,8 +184,9 @@ export const Navigation = () => {
             >
               <TrashBox />
             </PopoverContent>
-          </Popover> */}
-        </div>
+          </Popover>
+        </div> */}
+
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
